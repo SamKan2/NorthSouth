b42182304ea632a00ab7201855a947ccd739e137
Sam <samuelpkantor@gmail.com>
2023-02-22 17:54:39 +0100

    added css to make responsive

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ toggles.forEach(toggle => {
         console.log('working')
     })
 });
+
+const toggle = document.getElementById('toggle-button')
+const nav = document.getElementById('nav')
+const navbarLinks = document.getElementById('navbar-links')
+
+toggle.addEventListener('click', () => {
+    nav.classList.toggle('active')
+    navbarLinks.classList.toggle('active')
+})
+
+
