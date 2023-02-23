041157a2cc89e109c14d59ac5b9101981ae294a3
Sam <samuelpkantor@gmail.com>
2023-02-23 16:11:52 +0100

    added more animation, fixed nav links

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 const toggles = document.querySelectorAll('.faq-toggle')
 
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
-        console.log('working')
     })
 });
 
@@ -15,5 +27,3 @@ toggle.addEventListener('click', () => {
     nav.classList.toggle('active')
     navbarLinks.classList.toggle('active')
 })
-
-
